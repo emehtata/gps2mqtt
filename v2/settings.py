3470b58bf9e09225b0ad3e5470e4253359683787
Esa Mehtätalo <emehtata@users.noreply.github.com>
2024-05-23 20:47:12 +0300

    Paho-mqtt>=2.0 compatible

--- v2/settings.py
+++ v2/settings.py
@@ -1,6 +1,7 @@
 # MQTT brokers details
 brokers = [
     {'host': '192.168.7.186', 'port':  1883, 'client': None, 'connected': False }
+    # {'host': 'localhost', 'port': 1883, 'client': None, 'connected': False }
 ]
 
 
