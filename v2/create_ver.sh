47457345ae3156edc0e2f076463554dac3d401aa
Esa Mehtätalo <emehtata@users.noreply.github.com>
2024-05-27 10:14:31 +0300

    create version with date

--- v2/create_ver.sh
+++ v2/create_ver.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Get the current time in HHMMSS format
-current_time=$(date +"%y%m%d%H%M%S")
+cy=$(( $(date +"%Y")-2023 ))
+current_time=${cy}$(date +"%m%d%H%M")
 
 # Convert the time to a hexadecimal number
-hex_time=$(printf '%010x\n' "$current_time")
+hex_time=$(printf '%08x\n' "$current_time")
 
 # Print the hexadecimal number
 # echo "Current time in HHMMSS: $current_time"
